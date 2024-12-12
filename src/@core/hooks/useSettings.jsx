91b1d754d1e7db4563f2790503b3a9860e674651
Franc02306 <francomunoz230602@gmail.com>
2024-12-12 13:18:00 -0500

    estable WS | 9 (cambio de color estbale)

--- src/@core/hooks/useSettings.jsx
+++ src/@core/hooks/useSettings.jsx
@@ -1,8 +1,12 @@
 // React Imports
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 
 // Context Imports
 import { SettingsContext } from '@core/contexts/settingsContext'
+
+import primaryColorConfig from '@configs/primaryColorConfig'
+
+const defaultPrimaryColor = primaryColorConfig.find(item => item.name === 'primary-2')?.main
 
 export const useSettings = () => {
   // Hooks
@@ -11,6 +15,15 @@ export const useSettings = () => {
   if (!context) {
     throw new Error('useSettingsContext must be used within a SettingsProvider')
   }
+
+  const { settings, updateSettings } = context
+
+  // Forzar el color predeterminado si no está presente en la configuración
+  useEffect(() => {
+    if (!settings.primaryColor || settings.primaryColor !== defaultPrimaryColor) {
+      updateSettings({ primaryColor: defaultPrimaryColor })
+    }
+  }, [settings, updateSettings])
 
   return context
 }
