53b90b80b61d8bb53c3bbe4e53b6f91601220147
Franc02306 <francomunoz230602@gmail.com>
2025-01-17 10:04:50 -0500

    cambios

--- src/app/[lang]/(dashboard)/pages/user-profile/page.jsx
+++ src/app/[lang]/(dashboard)/pages/user-profile/page.jsx
@@ -7,9 +7,10 @@ import dynamic from 'next/dynamic'
 import { useSession } from 'next-auth/react'
 
 // Component Imports
-import UserProfile from '@views/pages/user-profile'
+// import UserProfile from '@views/pages/user-profile'
+import UserProfile from '../../../../../views/pages/user-profile/index'
 
-const ProfileTab = dynamic(() => import('@views/pages/user-profile/profile'))
+const ProfileTab = dynamic(() => import('../../../../../views/pages/user-profile/profile/index'))
 
 const ProfilePage = () => {
   const { data: session, status } = useSession()
